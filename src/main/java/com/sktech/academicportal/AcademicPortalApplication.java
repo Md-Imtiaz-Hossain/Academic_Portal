4e99e33479911073995411be79618287de46a69f
Imtiaz Hossain <imtiaz15-11790@diu.edu.bd>
2022-06-19 19:52:12 +0600

    Project Created.

---
+++ src/main/java/com/sktech/academicportal/AcademicPortalApplication.java
@@ -0,0 +1,13 @@
+package com.sktech.academicportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sktech/academicportal/AcademicPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sktech.academicportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
