8d8b5affbbdd53b945fb3b01b6ebb7614886c941
Imtiaz Hossain <imtiaz15-11790@diu.edu.bd>
2022-07-22 20:43:19 +0600

    Preparing for deploy.

--- src/main/java/com/sktech/academicportal/controllers/MainController.java
+++ src/main/java/com/sktech/academicportal/controllers/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String viewHomePage(Model model) {
         model.addAttribute("pageTitle", "Homepage");
         return "/home-page";
