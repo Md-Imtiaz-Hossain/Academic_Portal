c5928ac0895a93f6cee3ef398219d020e7b68a8c
Imtiaz Hossain <imtiaz15-11790@diu.edu.bd>
2022-07-24 16:38:09 +0600

    Request mapping add in home controller.

--- src/main/java/com/sktech/academicportal/controllers/MainController.java
+++ src/main/java/com/sktech/academicportal/controllers/MainController.java
@@ -1,18 +1,16 @@
 package com.sktech.academicportal.controllers;
 
-import com.sktech.academicportal.controllers.allusers.LoggedInUser;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class MainController {
 
-    @Autowired
-    LoggedInUser loggedInUser;
 
-    @GetMapping("/")
+    @GetMapping("/view")
     public String viewHomePage(Model model) {
         model.addAttribute("pageTitle", "Homepage");
         return "home-page";
