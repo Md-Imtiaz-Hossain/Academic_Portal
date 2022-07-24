be1100ca8eae3a2df7884ea8f75f3b0b14691839
Imtiaz Hossain <imtiaz15-11790@diu.edu.bd>
2022-07-24 16:46:01 +0600

    Request mapping delete in home controller.

--- src/main/java/com/sktech/academicportal/controllers/MainController.java
+++ src/main/java/com/sktech/academicportal/controllers/MainController.java
@@ -3,14 +3,12 @@ package com.sktech.academicportal.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
 public class MainController {
 
 
-    @GetMapping("/view")
+    @GetMapping("/")
     public String viewHomePage(Model model) {
         model.addAttribute("pageTitle", "Homepage");
         return "home-page";
