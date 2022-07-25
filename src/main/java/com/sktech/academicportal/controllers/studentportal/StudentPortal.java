77100d3a386573e07e4a9f3738e3e7954444bf3c
Imtiaz Hossain <imtiaz15-11790@diu.edu.bd>
2022-07-25 15:21:16 +0600

    Result view from logged-in Student portal. And doing some fragments refactoring.

--- src/main/java/com/sktech/academicportal/controllers/studentportal/StudentPortal.java
+++ src/main/java/com/sktech/academicportal/controllers/studentportal/StudentPortal.java
@@ -1,6 +1,8 @@
 package com.sktech.academicportal.controllers.studentportal;
 
+import com.sktech.academicportal.entity.StudentResult;
 import com.sktech.academicportal.entity.User;
+import com.sktech.academicportal.service.ResultService;
 import com.sktech.academicportal.service.RoutineService;
 import com.sktech.academicportal.service.SubjectService;
 import com.sktech.academicportal.service.UserService;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/student-portal")
@@ -25,7 +30,11 @@ public class StudentPortal {
     @Autowired
     SubjectService subjectService;
 
+    @Autowired
+    ResultService resultService;
 
+
+    // Logged in Student all subject class routine
     @GetMapping("/my-routine")
     public String myRoutine(Model model, Principal principal) {
         String userEmail = principal.getName();
@@ -38,5 +47,28 @@ public class StudentPortal {
 
         return "studentportal/my-routine";
     }
+
+    // Logged in Student all subject result
+    @GetMapping("/my-result")
+    public String myResult(Model model, Principal principal) {
+
+        String userEmail = principal.getName();
+        User user = userService.getUserByEmail(userEmail);
+
+        Set<StudentResult> results = new HashSet<>();
+        List<StudentResult> resultList = resultService.getAllResul();
+        System.out.println(resultList);
+        for (StudentResult studentResult : resultList) {
+            if (studentResult.getUserId().equals(user.getId())) {
+                results.add(studentResult);
+            }
+        }
+
+        model.addAttribute("pageTitle", "Mark List with Username and Subject name");
+        model.addAttribute("mark", results);
+
+        return "studentportal/my-result";
+    }
+
 
 }
