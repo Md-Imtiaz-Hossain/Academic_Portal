78f093761abc7896d29fcc560e9f36a3d81ae33d
Imtiaz Hossain <imtiaz15-11790@diu.edu.bd>
2022-08-04 23:45:48 +0600

    Add logout function. Bugfix on log in page design. Now in login page design will appear.

--- src/main/java/com/sktech/academicportal/controllers/MainController.java
+++ src/main/java/com/sktech/academicportal/controllers/MainController.java
@@ -1,9 +1,5 @@
 package com.sktech.academicportal.controllers;
 
-import com.sktech.academicportal.controllers.profile.ProfileUpdate;
-import com.sktech.academicportal.entity.Profile;
-import com.sktech.academicportal.entity.Role;
-import com.sktech.academicportal.entity.User;
 import com.sktech.academicportal.repositories.RoleRepository;
 import com.sktech.academicportal.service.ProfileService;
 import com.sktech.academicportal.service.UserService;
@@ -13,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
-import java.util.Set;
 
 @Controller
 public class MainController {
--- src/main/java/com/sktech/academicportal/security/WebSecurityConfig.java
+++ src/main/java/com/sktech/academicportal/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.sktech.academicportal.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -43,13 +45,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // to make allow public access without authentication
-        /*http.headers().frameOptions().sameOrigin();
-        http.csrf().disable();*/
+
+        String[] staticResources = {"/css/**", "/images/**", "/fonts/**", "/scripts/**", "/plugins/**",
+                " /resources/**", "/js/**", "/login", "/api/**", "/", "/static/**", "/dist/css/**", "/dist/js/**", "/dist/img/**", "/dist/**"};
 
 
         http.authorizeRequests()
-                .antMatchers("/images/**", "/css/**", "/js/**", "/login", "/api/**").permitAll()
+                .antMatchers(staticResources).permitAll()
+                .antMatchers("/forgot-password", "/change-password").permitAll()
                 .antMatchers("/users/**").hasAnyAuthority("Admin")
                 .antMatchers("/").permitAll()
                 .antMatchers("/login").permitAll()
@@ -61,33 +64,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .clearAuthentication(true)
                 .permitAll()
-                .logoutSuccessUrl("/login")
                 .and().rememberMe().key("AbcDefgHijKlmnOpqrs_1234567890") //this will be create jsession id (cookies) when login
                 .tokenValiditySeconds(7 * 24 * 60 * 60);
 
-
-
-
-//        http.authorizeRequests().anyRequest().permitAll();
-
-//        form th:action="@{/logout} will automatic map with spring authen logout?
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-//        web.ignoring()
-//                .antMatchers("/h2-console/**")
-//                .antMatchers("/images/**", "/js/**", "/webjars/**");
 
-
+        web.ignoring().antMatchers("/css/**");
+        web.ignoring().antMatchers("/scripts/**");
+        web.ignoring().antMatchers("/images/**");
+        web.ignoring().antMatchers("/js/**");
+        web.ignoring().antMatchers("/webjars/**");
 
 
     }
 
-    //configure(AuthenticationManagerBuilder auth) -> daoAuthenticationProvider -> use bean UserDetailsService userDetailsService()
-    // -> configure(HttpSecurity http) -> configure(WebSecurity web)
-    // -> khi loggin thi lay info tu authenticationProvider
-    // sau do lay data ShopmeUserDetailsService#loadUserByUsername will invoke -> pass username/password tu user fromdb
-    // then check
+
 }
