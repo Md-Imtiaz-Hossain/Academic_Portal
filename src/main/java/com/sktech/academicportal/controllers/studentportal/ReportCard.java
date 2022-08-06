fff1623a3adae67f79f7d1d1dbd5576677dd75c6
Imtiaz Hossain <imtiaz15-11790@diu.edu.bd>
2022-08-06 23:40:03 +0600

    Data render into form.

--- src/main/java/com/sktech/academicportal/controllers/studentportal/ReportCard.java
+++ src/main/java/com/sktech/academicportal/controllers/studentportal/ReportCard.java
@@ -1,19 +1,27 @@
 package com.sktech.academicportal.controllers.studentportal;
 
 
+import com.sktech.academicportal.service.ResultService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.persistence.Access;
 import java.security.Principal;
 
 @Controller
 @RequestMapping("/report-card")
 public class ReportCard {
 
+    @Autowired
+    ResultService resultService;
+
     @GetMapping("/view")
     public String cardHome(Model model, Principal principal){
+        model.addAttribute("pageTitle", "Mark List with user and subject");
+        model.addAttribute("mark", resultService.getAllResulWithGrade());
         return "/studentportal/student-report-card";
     }
 
