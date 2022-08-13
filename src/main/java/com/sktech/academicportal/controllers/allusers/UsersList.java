7e91ab07e814606dc6bbd70a359a9df2d021bd7a
Imtiaz Hossain <imtiaz15-11790@diu.edu.bd>
2022-08-13 23:40:21 +0600

    User update with picture.

--- src/main/java/com/sktech/academicportal/controllers/allusers/UsersList.java
+++ src/main/java/com/sktech/academicportal/controllers/allusers/UsersList.java
@@ -1,15 +1,12 @@
 package com.sktech.academicportal.controllers.allusers;
 
-import com.sktech.academicportal.entity.Profile;
 import com.sktech.academicportal.entity.Role;
 import com.sktech.academicportal.entity.User;
 import com.sktech.academicportal.helper.FileUploadUtil;
-import com.sktech.academicportal.helper.Message;
 import com.sktech.academicportal.repositories.UserRepository;
 import com.sktech.academicportal.service.ProfileService;
 import com.sktech.academicportal.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -17,9 +14,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.io.EOFException;
 import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
@@ -30,6 +25,7 @@ public class UsersList {
 
     @Autowired
     UserService userService;
+
     @Autowired
     ProfileService profileService;
     @Autowired
@@ -42,8 +38,7 @@ public class UsersList {
         return userService.getUserByEmail(principal.getName());
     }
 
-
-    // View All User store in DB with Datatable
+    // User List
     @GetMapping("/list")
     public String viewUserListPage(Model model) {
         model.addAttribute("pageTitle", "User List");
@@ -51,22 +46,18 @@ public class UsersList {
         return "backenduserslist/user-list";
     }
 
-    // For create a new User. Open a from for create new Student
+    // Create a new User
     @GetMapping("/new")
     public String addNewUserForm(Model model) {
-
         User user = new User();
         List<Role> listRoles = userService.listRoles();
-
         model.addAttribute("pageTitle", "User Registration");
         model.addAttribute("listRoles", listRoles);
         model.addAttribute("user", user);
-
         return "backenduserslist/user-new-form";
     }
 
-
-    // Process the fill up form after save button clicked.
+    // Process save user
     @PostMapping("/save")
     public String processNewUserForm(@Valid @ModelAttribute("user") User user,
                                      @RequestParam("image") MultipartFile multipartFile,
@@ -105,46 +96,36 @@ public class UsersList {
         return "redirect:/user/list";
     }
 
-
-    // Open the Update form for person Information updating
+    // Update user form
     @GetMapping("/edit/{id}")
     public String editUserForm(@PathVariable Integer id, Model model) {
-
         List<Role> listRoles = userService.listRoles();
-
         model.addAttribute("pageTitle", "Edit  User Information");
         model.addAttribute("user", userService.getUserById(id));
         model.addAttribute("listRoles", listRoles);
-
         return "backenduserslist/user-update-form";
     }
 
-
-    // Process the updated information after update button clicked.
+    // Process user update
     @PostMapping("/update/{id}")
     public String updateUser(@PathVariable Integer id, @ModelAttribute("user") User user, Model model) {
-
         User existingUser = userService.getUserById(id);
         existingUser.setId(id);
         existingUser.setFirstName(user.getFirstName());
         existingUser.setLastName(user.getLastName());
         existingUser.setEmail(user.getEmail());
         existingUser.setRoles(user.getRoles());
-
         String encodedPassword = userService.encodePasswordUsingString(user.getPassword());
         existingUser.setPassword(encodedPassword);
-
         userService.updateUser(existingUser);
         return "redirect:/user/list";
     }
 
-
-    // Delete the person information and confirm before delete.
+    // Delete user
     @GetMapping("/delete/{id}")
     public String deleteUser(@PathVariable Integer id) {
         userService.deleteUserById(id);
         return "redirect:/user/list";
     }
-
 
 }
