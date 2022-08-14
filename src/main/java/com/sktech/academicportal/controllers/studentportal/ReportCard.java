e97306a4de78dc0b2c8f89fb1303d18b3ac80a5e
Imtiaz Hossain <imtiaz15-11790@diu.edu.bd>
2022-08-15 04:56:55 +0600

    Deploy.

--- src/main/java/com/sktech/academicportal/controllers/studentportal/ReportCard.java
+++ src/main/java/com/sktech/academicportal/controllers/studentportal/ReportCard.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.persistence.Access;
 import java.security.Principal;
 
 @Controller
